0411d50006f550b919332b206d7cdd190ef8a566
Sriharshini04 <harshini@synthlake.com>
2026-01-30 11:42:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,4 +38,7 @@ if submit_button:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json)
-sf_df=st.dataframe(data=smoothiefroot_response.json,use_container_width=True)
+sf_df = st.dataframe(
+    data=smoothiefroot_response.json(),
+    use_container_width=True
+)
