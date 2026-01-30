8f42296585168b487497156c317c48493ddd719f
Sriharshini04 <harshini@synthlake.com>
2026-01-30 12:21:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ st.write('The name on your Smoothie will be:',name_on_order)
 
 cnx=st.connection("snowflake")
 session=cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))\.to_pandas()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')).to_pandas()
 ingredients_list=st.multiselect(
     'Choose up to 5 ingredients:'
     ,my_dataframe
@@ -73,7 +73,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
       ingredients_string += fruit_chosen + ' '
 
-      st.subheader(fruit_chosen + ' Nutrition Information')
+      st.subheader(fruit_chosen + " Nutrition Information")
 
       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
 
